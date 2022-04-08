67d40c751f8c3e9c2ec08ea6c64d891e48a78de7
Ninh Tran <mr.tdnn@gmail.com>
2022-04-08 06:00:54 +0000

    Adding and updating "waitUntilConfirmed" utility function to SDK 1.3.2

--- code/week02/createDid/app/src/main/kotlin/createDid/App.kt
+++ code/week02/createDid/app/src/main/kotlin/createDid/App.kt
@@ -25,3 +25,34 @@ fun main(args: Array<String>) {
     println("long form: $didLongForm")
     println()
 }
+
+// Waits until an operation is confirmed by the Cardano network.
+// NOTE: Confirmation doesn't necessarily mean that operation was applied. 
+// For example, it could be rejected because of an incorrect signature or other reasons.
+fun waitUntilConfirmed(nodePublicApi: NodePublicApi, operationId: AtalaOperationId) {
+    var status = runBlocking {
+        nodePublicApi.getOperationStatus(operationId)
+    }
+    while (status != AtalaOperationStatus.CONFIRMED_AND_APPLIED &&
+        status != AtalaOperationStatus.CONFIRMED_AND_REJECTED
+    ) {
+        println("Current operation status: ${AtalaOperationStatus.asString(status)}")
+        Thread.sleep(1000)
+        status = runBlocking {
+            nodePublicApi.getOperationStatus(operationId)
+        }
+    }
+}
+
+// Creates a list of potentially useful keys out of a mnemonic code
+fun prepareKeysFromMnemonic(mnemonic: MnemonicCode, pass: String): Map<String, ECKeyPair> {
+    val seed = KeyDerivation.binarySeed(mnemonic, pass)
+    val issuerMasterKeyPair = KeyGenerator.deriveKeyFromFullPath(seed, 0, MasterKeyUsage, 0)
+    val issuerIssuingKeyPair = KeyGenerator.deriveKeyFromFullPath(seed, 0, IssuingKeyUsage, 0)
+    val issuerRevocationKeyPair = KeyGenerator.deriveKeyFromFullPath(seed, 0, RevocationKeyUsage, 0)
+    return mapOf(
+        Pair(PrismDid.DEFAULT_MASTER_KEY_ID, issuerMasterKeyPair),
+        Pair(PrismDid.DEFAULT_ISSUING_KEY_ID, issuerIssuingKeyPair),
+        Pair(PrismDid.DEFAULT_REVOCATION_KEY_ID, issuerRevocationKeyPair)
+    )
+}
--- code/week04/issue/app/src/main/kotlin/issue/App.kt
+++ code/week04/issue/app/src/main/kotlin/issue/App.kt
@@ -49,6 +49,19 @@ fun waitUntilConfirmed(nodePublicApi: NodePublicApi, operationId: AtalaOperation
     }
 }
 
+// Creates a list of potentially useful keys out of a mnemonic code
+fun prepareKeysFromMnemonic(mnemonic: MnemonicCode, pass: String): Map<String, ECKeyPair> {
+    val seed = KeyDerivation.binarySeed(mnemonic, pass)
+    val issuerMasterKeyPair = KeyGenerator.deriveKeyFromFullPath(seed, 0, MasterKeyUsage, 0)
+    val issuerIssuingKeyPair = KeyGenerator.deriveKeyFromFullPath(seed, 0, IssuingKeyUsage, 0)
+    val issuerRevocationKeyPair = KeyGenerator.deriveKeyFromFullPath(seed, 0, RevocationKeyUsage, 0)
+    return mapOf(
+        Pair(PrismDid.DEFAULT_MASTER_KEY_ID, issuerMasterKeyPair),
+        Pair(PrismDid.DEFAULT_ISSUING_KEY_ID, issuerIssuingKeyPair),
+        Pair(PrismDid.DEFAULT_REVOCATION_KEY_ID, issuerRevocationKeyPair)
+    )
+}
+
 val environment = "ppp.atalaprism.io"
 val grpcOptions = GrpcOptions("https", environment, 50053)
 val nodeAuthApi = NodeAuthApiImpl(GrpcOptions("https", environment, 50053))
